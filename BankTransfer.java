f5c2755fdfbf6fb06d6301f62aff3294519a37ce
Nura20 <162921404+Nura20@users.noreply.github.com>
2024-03-10 23:51:49 +0300

    Add files via upload

---
+++ BankTransfer.java
@@ -0,0 +1,30 @@
+package lap_mid1;
+
+public class BankTransfer extends payment{
+
+	private String BankName;
+	private String TranserNum;
+	
+	public BankTransfer(double a, String bName,String tNum) {
+		super(a);
+		BankName=bName;
+		TranserNum= tNum;
+		
+	}
+	public BankTransfer(BankTransfer BT) {
+		super(BT.amount);
+		BankName=BT.BankName;
+		TranserNum=BT.TranserNum;
+	}
+public double CalculateGrand(){
+	amount= amount+ (amount * (4.78/100));
+	return amount;
+}
+	
+	public void Displayinfo() {
+		super.Displayinfo();
+		System.out.println("Bank Name : " + BankName + " , TranserNum : " +
+				TranserNum ) ;
+	}
+
+}
